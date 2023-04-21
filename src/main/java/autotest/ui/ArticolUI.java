52bb892e988e19bf97629cf1aefc0de639315c1f
predaalex <preda.alex30@yahoo.com>
2023-04-21 12:57:24 +0300

    testare modificare articol

--- src/main/java/autotest/ui/ArticolUI.java
+++ src/main/java/autotest/ui/ArticolUI.java
@@ -4,4 +4,10 @@ import org.openqa.selenium.By;
 
 public class ArticolUI {
     public By creazaArticolButon = By.xpath("//a[@href=\"/create-articol\"]");
+
+    public By editeazaArticolButon = By.xpath("//button[@class=\"mat-focus-indicator mat-flat-button mat-button-base mat-primary\"]");
+
+    public By salveazaModificarileButon = By.xpath("//button[@class=\"mat-focus-indicator mat-flat-button mat-button-base mat-primary\"]");
+
+    public By mdText = By.xpath("//markdown");
 }
